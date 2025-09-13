af300f003cac5e027abacaf85ff5f8613a776d91
Dang To <206424389+yomsi-thd@users.noreply.github.com>
2025-09-13 09:07:05 +0700

    Create a function that returns correct grid size
    When the user click on a button, the prompt will allow the user to type
    in any value they could type on their keyboard. This needs to be
    restricted because there's no way to produce a negative, decimal, or
    string based grid size. Thus, this function is used to reduce the
    chances that a weird error-inducing values will happen.

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
 //Create square divs and put the squares inside a "container" div
 const container = document.querySelector(".container");
-let defaultGridSize = 16 ** 2;
+let defaultGridSize = 16;
 
 function createSquareDivGrid(userInput){
     numberOfSquareDivsToAdd = userInput ** 2;
     for (let i = 1; i <= numberOfSquareDivsToAdd; i++){
         const squareDiv = document.createElement('div');
         squareDiv.setAttribute("class", "square");
-        squareDiv.style.width = `${960/16}px`;
-        squareDiv.style.height = `${960/16}px`;
+        squareDiv.style.width = `${960/userInput}px`;
+        squareDiv.style.height = `${960/userInput}px`;
         container.appendChild(squareDiv);
     };
 }
@@ -19,7 +19,17 @@ function removeSquareDivGrid(){
     squareDivGrid.forEach((squareDiv) => container.removeChild(squareDiv));
 }
 
+function getUserDesiredGridSize(){
+    let userInput = parseInt(prompt("Please enter your desired grid size (min: 1 | max: 100): "));
+    while (!Number.isInteger(userInput) || userInput < 1 || userInput > 100) {
+        userInput = parseInt(prompt("Please enter your desired grid size (min: 1 | max: 100): "));
+    }
+    return userInput;
+}
+
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
+    let gridSize = getUserDesiredGridSize();
     removeSquareDivGrid();
-})
+    createSquareDivGrid(gridSize);
+});
