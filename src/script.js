dd75817d341183d7bfd2b7c4cb1e5e51acaed994
Dang To <206424389+yomsi-thd@users.noreply.github.com>
2025-09-13 08:24:49 +0700

    Put code that create/remove the grid in functions
    The site now has a button that allows the user to enter the number of
    squares per side for their desired grid. The previous code only dealt
    with 1 size (16x16). Now, the code is more flexible by being in a
    function, allowing the program to easily make a grid in all sort of
    sizes (cap to 100 in the future);
    
    To make space for new squares to be generated, the old squares generated
    from the old grid needs to be remove. Since we want this to run on every
    button press, it's put inside a function for simplicity.

--- src/script.js
+++ src/script.js
@@ -1,10 +1,25 @@
 //Create square divs and put the squares inside a "container" div
 const container = document.querySelector(".container");
-let numberOfSquareDivsToAdd = 16 * 16;
-for (let i = 1; i <= numberOfSquareDivsToAdd; i++){
-    const squareDiv = document.createElement('div');
-    squareDiv.setAttribute("class", "square");
-    squareDiv.style.width = `${960/16}px`;
-    squareDiv.style.height = `${960/16}px`;
-    container.appendChild(squareDiv);
-};
+let defaultGridSize = 16 ** 2;
+
+function createSquareDivGrid(userInput){
+    numberOfSquareDivsToAdd = userInput ** 2;
+    for (let i = 1; i <= numberOfSquareDivsToAdd; i++){
+        const squareDiv = document.createElement('div');
+        squareDiv.setAttribute("class", "square");
+        squareDiv.style.width = `${960/16}px`;
+        squareDiv.style.height = `${960/16}px`;
+        container.appendChild(squareDiv);
+    };
+}
+createSquareDivGrid(defaultGridSize); // The default grid size is 16x16
+
+function removeSquareDivGrid(){
+    const squareDivGrid = document.querySelectorAll(".square");
+    squareDivGrid.forEach((squareDiv) => container.removeChild(squareDiv));
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    removeSquareDivGrid();
+})
